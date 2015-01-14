12a234cd044323700f7ae7fca0d31038f70596a8
Alexander Hjelm <nick.nertova@gmail.com>
2015-01-14 21:38:43 +0100

    Bases now show num of units, counting up w/ time

---
+++ Assets/Base.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Base : MonoBehaviour {
+
+	public int NumberOfUnits = 0;
+	public int MaxNumberOfUntis = 10;
+
+	float repeatingTime = 1f;
+
+	//Scripts on this
+	Owner ownerScript;
+
+	//UI refs
+	Text numOfShipsText;
+
+	//Other GOs
+	GameObject playerHandler;
+	Color[] playerCols;
+
+	void Awake()
+	{
+		//refs
+		numOfShipsText = this.GetComponentInChildren<Text> ();
+		ownerScript = this.GetComponent<Owner> ();
+
+		playerHandler = GameObject.FindGameObjectWithTag ("PlayerHandler");
+		playerCols = playerHandler.GetComponent<PlayerHandler> ().playerCols;
+	}
+
+	void Start()
+	{
+		InvokeRepeating ("AddUnit", 0f, repeatingTime);	//Incr unit count over time, cancel invoke and invoke again to change
+		numOfShipsText.color = playerCols [ownerScript.owner];	//Change color of GUI text to match that of owner
+	}
+
+	void Update()
+	{
+		numOfShipsText.text = NumberOfUnits.ToString();
+	}
+
+	void AddUnit()
+	{
+		if (NumberOfUnits < MaxNumberOfUntis)
+		{
+			NumberOfUnits++;
+		}
+	}
+}
