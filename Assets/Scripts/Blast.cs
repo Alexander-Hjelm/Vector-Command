bc61a33c4520886ddee4d32e54b7092b4c26e8a8
Alexander Hjelm <nick.nertova@gmail.com>
2015-01-16 19:55:16 +0100

    Ship has combat target, projectile moves and dies after 10s

---
+++ Assets/Scripts/Blast.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blast : MonoBehaviour {
+
+	float speed = 10;
+
+	// Use this for initialization
+	void OnEnable () {
+		Invoke ("TimedDestroy", 10f);	//failsafe
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.up * speed * Time.deltaTime;
+	}
+
+	void TimedDestroy()
+	{
+		this.gameObject.SetActive(false);
+	}
+
+}
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -7,21 +7,36 @@ public class Ship : MonoBehaviour {
 	Hp hpScript;
 
 	//vars
-	public Vector3 target;
+	public Vector3 objective;	//target of lifetime
+	public Vector3 target;	//momentary target
+	public GameObject combatTarget;
 	float speed;
 	float topSpeed = 0.05f;	//Settable from outside
 	float turnSpeed = 2.5f; //Settable from outside
 
-	bool inCombat = false;
+	public bool inCombat = false;
 
 	void Start () {
 		hpScript = this.GetComponent <Hp>();
+	}
+	
+	void OnEnable()
+	{
 		speed = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if (inCombat && combatTarget)
+		{
+			target = combatTarget.transform.position;
+		}
+		else
+		{
+			target = objective;
+		}
+
 		//Movement
 		speed = Mathf.Lerp (speed, topSpeed, Time.deltaTime * 0.7f);
 		this.transform.position += transform.up * speed;
