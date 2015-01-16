7095dce097c94e409b98af38be3e402a4ec60073
Alexander Hjelm <nick.nertova@gmail.com>
2015-01-16 14:29:05 +0100

    Ships are now destroyed when hp reaches 0

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+	Hp hpScript;
+
+	// Use this for initialization
+	void Start () {
+		hpScript = this.GetComponent <Hp>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (hpScript.hp <= 0)
+		{
+			Destroy(this.gameObject);
+		}
+
+	}
+}
