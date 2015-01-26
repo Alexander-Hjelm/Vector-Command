8f15c83a90cbcbd688e80cfd6cbc4c3637834a60
Alexander Hjelm <nick.nertova@gmail.com>
2015-01-26 21:14:17 +0100

    Fixed pos and rot of ships being spawned

--- Assets/ShipSpawner.cs
+++ Assets/ShipSpawner.cs
@@ -16,6 +16,8 @@ public class ShipSpawner : MonoBehaviour {
 	public Vector3 mousePos;
 	public GameObject hoverTarget;
 
+	public bool spawning = false;
+
 	void Awake ()
 	{
 		baseScript = gameObject.GetComponent<Base> ();
@@ -49,8 +51,12 @@ public class ShipSpawner : MonoBehaviour {
 			hoverTarget = GetCollision();
 			if (hoverTarget != null && hoverTarget != colliderObj3D)
 			{
-				Invoke("SpawnShipInvoked", 0.2f);
+				SpawnShipInvoked();
+				CalcRandSpawnPos(hoverTarget.transform.position);
 			}
+
+
+
 
 			//If, then invoke spawn ship w/ other base as target
 
@@ -61,8 +67,12 @@ public class ShipSpawner : MonoBehaviour {
 
 	void SpawnShipInvoked()
 	{
-		print (hoverTarget.transform.parent.position);
-		baseScript.SpawnUnit(this.transform.position + Vector3.up * 2, Quaternion.identity, hoverTarget.transform.parent.position);
+		Vector3 pos = CalcRandSpawnPos(hoverTarget.transform.position);
+		Quaternion rot = Quaternion.LookRotation (transform.position - pos, Vector3.forward);
+		rot.x = 0;
+		rot.y = 0;
+
+		baseScript.SpawnUnit(pos, rot, hoverTarget.transform.parent.position);
 	}
 
 	Vector3 ClickPos2World()
@@ -85,5 +95,27 @@ public class ShipSpawner : MonoBehaviour {
 			return hit.collider.gameObject;
 		}
 		else return null;
+	}
+
+	Vector3 CalcRandSpawnPos(Vector3 target)	//Using trigonometry, find the point towards target base that is 1.3 units away    +    add a little random
+	{
+		float angle = Vector3.Angle(-Vector3.left, target - transform.position);
+
+		angle = 2 * Mathf.PI * angle / 360;	//to radians
+
+		Debug.DrawRay (transform.position, target - transform.position);
+		Debug.DrawRay (transform.position, -Vector3.left);
+
+		//angle could be negative
+		Vector3 cross = Vector3.Cross(-Vector3.left, target - transform.position);
+		if (cross.z < 0)
+			angle = -angle;
+
+		angle += Random.Range (-Mathf.PI / 3, Mathf.PI / 3);
+
+		Debug.Log (angle);
+
+		Vector3 spawnPos = 1.3f * new Vector3 (Mathf.Cos (angle), Mathf.Sin (angle), 0);
+		return transform.position + spawnPos;
 	}
 }
