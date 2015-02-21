06d34689a64437468601c3888afaac5bd6b942cc
Alexander Hjelm <nick.nertova@gmail.com>
2015-02-21 21:40:47 +0100

    Particle effect on ship destruction, backgroung

--- Assets/BlastCollisionController.cs
+++ Assets/BlastCollisionController.cs
@@ -9,25 +9,58 @@ public class BlastCollisionController : MonoBehaviour {
 	public float repTime = 0.1f;	//InvokeRep rate
 	public float threshDist = 0.2f;	//max dist for collision to occur
 
+	public GameObject particlePool;
+
+	bool collided;	//for avoiding spawning multiple particle systems
+
 	void Awake () {
 		blastScript = this.GetComponent<Blast> ();
+
+		particlePool = GameObject.FindGameObjectWithTag ("ParticlePool");
 	}
 	
 	void OnEnable()
 	{
 		InvokeRepeating ("CheckCollision", 0f, repTime);
+		collided = false;
 	}
 	
 	void CheckCollision()	//Main
 	{
-		if(blastScript.combatTarget != null)
+		if(!collided && blastScript.combatTarget != null)
 		{
 			if((this.transform.position - blastScript.combatTarget.transform.position).magnitude < threshDist)
 			{
+				collided = true;
+
 				blastScript.combatTarget.GetComponent<Hp>().modHp(-4);		//deal dmg
+
+				if(blastScript.combatTarget.GetComponent<Hp>().hp <= 0)
+				{
+					//Spawn particle system
+					InstParticleSystem();
+					print("Dead!");
+				}
+
 				gameObject.SetActive(false);				//Inactivate
 			}
 		}
+	}
+
+	void InstParticleSystem()
+	{
+		GameObject partSys = particlePool.GetComponent<ObjectPoolerScript> ().GetAvailablePooledObject ();
+		partSys.transform.position = blastScript.combatTarget.transform.position;
+
+		Quaternion partRot = partSys.transform.rotation = this.transform.rotation;
+		partRot.x = 0.0f;	//only rot in z
+		partRot.y = 0.0f;
+		partSys.transform.rotation = partRot;
+
+		//Set color to target ship
+		partSys.GetComponent<ParticleSystem> ().startColor = blastScript.combatTarget.GetComponent<SpriteRenderer>().color;
+
+		partSys.SetActive (true);
 	}
 
 }
---
+++ Assets/ParticleDestroySelf.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleDestroySelf : MonoBehaviour {
+
+	public float disableTime = 1;
+
+	// Use this for initialization
+	void OnEnable () {
+		Invoke ("DisableSelf", disableTime);
+	}
+
+	void DisableSelf()
+	{
+		gameObject.SetActive (false);
+	}
+}
